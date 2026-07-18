f0b6a2f49277e4993f38d43ed89dde152264bbbf
Jhose <jhosepjamil@gmail.com>
2026-07-18 15:42:08 -0500

    refactor: comment out deprecated projects and add status type system
    - Comment out Green Sprout Innovations and ThinkUp SkillShare projects
    - Replace old status types (active/production) with 4 new types:
      completed, in_development, maintenance, private
    - Update i18n status labels for all project items
    - Set FyM statusKey to completed

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -1,42 +1,45 @@
 import {Icon} from '@iconify/react';
-import ecoCropImg from '../assets/projects/green_sprout_innovations-ecocrop.png';
-import skillShareImg from '../assets/projects/thinkup-skillshare.png';
+// import ecoCropImg from '../assets/projects/green_sprout_innovations-ecocrop.png';
+// import skillShareImg from '../assets/projects/thinkup-skillshare.png';
 import fymImg from '../assets/projects/fym.png';
 import tmdbLogo from '../assets/projects/external_logos/tmdb_api.svg';
 import React from "react";
 
-export const PROJECTS_DATA = [{
-    id: 1,
-    category: 'web',
-    statusKey: 'production',
-    title: 'Green Sprout Innovations - EcoCrop',
-    image: ecoCropImg,
-    technologies: [
-        {name: 'HTML5', icon: <Icon icon="simple-icons:html5"/>, color: '#E34F26'},
-        {name: 'CSS3', icon: <Icon icon="simple-icons:css3"/>, color: '#2965F1'},
-        {name: 'JavaScript', icon: <Icon icon="simple-icons:javascript"/>, color: '#F7DF1E'},
-        {name: 'Git', icon: <Icon icon="simple-icons:git"/>, color: '#F05032'},],
-    github: 'https://github.com/GreenSprout-Innovations/greensprout-innovations.github.io',
-    live: 'https://greensprout-innovations.github.io/',
-    labelTranslate: 'ecocrop'
-}, {
-    id: 2,
-    category: 'mobile',
-    statusKey: 'production',
-    title: 'ThinkUp - SkillShare',
-    image: skillShareImg,
-    technologies: [
-        {name: 'Flutter', icon: <Icon icon="simple-icons:flutter"/>, color: '#02569B'}, {
-        name: 'Kotlin', icon: <Icon icon="simple-icons:kotlin"/>, color: '#0095D5'},
-        {name: 'C#', icon: <Icon icon="simple-icons:cplusplus"/>, color: '#239120'},
-        {name: 'Git', icon: <Icon icon="simple-icons:git"/>, color: '#F05032'},],
-    github: 'https://github.com/orgs/ThinkUp-SkillShare/repositories',
-    live: 'https://appdistribution.firebase.dev/i/2bb66c756dd609c3',
-    labelTranslate: 'skillshare'
-}, {
+export const PROJECTS_DATA = [
+// {
+//     id: 1,
+//     category: 'web',
+//     statusKey: 'completed',
+//     title: 'Green Sprout Innovations - EcoCrop',
+//     image: ecoCropImg,
+//     technologies: [
+//         {name: 'HTML5', icon: <Icon icon="simple-icons:html5"/>, color: '#E34F26'},
+//         {name: 'CSS3', icon: <Icon icon="simple-icons:css3"/>, color: '#2965F1'},
+//         {name: 'JavaScript', icon: <Icon icon="simple-icons:javascript"/>, color: '#F7DF1E'},
+//         {name: 'Git', icon: <Icon icon="simple-icons:git"/>, color: '#F05032'},],
+//     github: 'https://github.com/GreenSprout-Innovations/greensprout-innovations.github.io',
+//     live: 'https://greensprout-innovations.github.io/',
+//     labelTranslate: 'ecocrop'
+// },
+// {
+//     id: 2,
+//     category: 'mobile',
+//     statusKey: 'completed',
+//     title: 'ThinkUp - SkillShare',
+//     image: skillShareImg,
+//     technologies: [
+//         {name: 'Flutter', icon: <Icon icon="simple-icons:flutter"/>, color: '#02569B'}, {
+//         name: 'Kotlin', icon: <Icon icon="simple-icons:kotlin"/>, color: '#0095D5'},
+//         {name: 'C#', icon: <Icon icon="simple-icons:cplusplus"/>, color: '#239120'},
+//         {name: 'Git', icon: <Icon icon="simple-icons:git"/>, color: '#F05032'},],
+//     github: 'https://github.com/orgs/ThinkUp-SkillShare/repositories',
+//     live: 'https://appdistribution.firebase.dev/i/2bb66c756dd609c3',
+//     labelTranslate: 'skillshare'
+// },
+{
     id:  4,
     category: 'web',
-    statusKey: 'production',
+    statusKey: 'completed',
     title: 'FyM - Find Your Movie',
     image: fymImg,
     technologies: [
