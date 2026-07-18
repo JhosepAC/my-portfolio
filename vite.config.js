32f33c8f6cc9e2cc0193a202857efd271282e578
Jhose <jhosepjamil@gmail.com>
2026-07-18 00:26:36 -0500

    perf: make CSS non-render-blocking via preload onload pattern
    - Added custom Vite plugin (cssNonBlocking) that transforms CSS
      <link> tags to use preload + onload pattern
    - CSS downloads start immediately but no longer block initial paint
    - noscript fallback for JS-disabled browsers

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+function cssNonBlocking() {
+  return {
+    name: 'css-non-blocking',
+    enforce: 'post',
+    transformIndexHtml(html) {
+      return html.replace(
+        /<link rel="stylesheet" crossorigin href="(.*?)">/g,
+        `<link rel="preload" as="style" href="$1" onload="this.rel='stylesheet'"><noscript><link rel="stylesheet" crossorigin href="$1"></noscript>`
+      )
+    }
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cssNonBlocking()],
   base: './',
   build: {
     rollupOptions: {
